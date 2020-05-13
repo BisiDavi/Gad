70abce5ac96ac51a7c63643f5cf1f03ed7f8a045
Olubisi David <oludavidconnect@gmail.com>
2020-05-13 05:13:59 +0100

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <div className="text">
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex numquam quaerat omnis amet placeat culpa voluptas aspernatur minus qui? Vero laborum illo placeat quaerat ab, dolorem mollitia nihil natus, esse rem perferendis iste voluptate distinctio ex libero. Dolores in nesciunt neque recusandae mollitia blanditiis hic vero, officia, est voluptatem sequi officiis deserunt possimus laborum, voluptate ducimus culpa voluptatum quam at doloremque similique aut rem! Provident cumque eligendi accusantium itaque quam beatae? Illum voluptates ea repudiandae aliquam deleniti asperiores accusantium veniam quae qui ducimus fugit inventore, non necessitatibus officia voluptas vitae nobis id? Nam soluta, doloribus dicta sit laborum animi iste.
-      </p>
+          </p>
         </div>
         <div className="about-image">
           <img src={aboutus} alt="about us" />
